a5b8b39bc4c482cde98845b80c6a9ac1751adb05
icecat <icecatm7@gmail.com>
2026-09-21 16:03:48 +0700

    Update FileItem.kt

--- app/src/main/java/com/zon/filemanager/FileItem.kt
+++ app/src/main/java/com/zon/filemanager/FileItem.kt
@@ -8,7 +8,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import java.io.File
 import java.text.DecimalFormat
 import kotlin.math.log10
 import kotlin.math.pow
